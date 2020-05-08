cd2b0ecd693f1a899fd90a85c173de9c2d942df0
MirZhou <mir3732@live.com>
2020-05-08 16:09:29 +0800

    first commit

---
+++ src/GuessNumber.java
@@ -0,0 +1,11 @@
+/**
+ * 猜数字游戏
+ * <p>Create time: 2020-05-08 15:51</p>
+ *
+ * @author 周光兵
+ **/
+public class GuessNumber {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
