c4479acb3ce5bff5c150e839d9a5097413af5644
mir3732 <mir3732@live.com>
2021-03-31 23:48:38 +0800

    新增测试线程上下文切换Demo

---
+++ src/cn/eros/thread/MultiThreadDemo.java
@@ -0,0 +1,91 @@
+
+/**
+ * 测试线程上下文切换Demo
+ */
+public class MultiThreadDemo {
+    public static void main(String[] args) {
+        ThreadContextSwitchTester multiThreadTester = new MultiThreadTester();
+        multiThreadTester.start();
+
+        ThreadContextSwitchTester serialtTester = new SerialTester();
+        serialtTester.start();
+    }
+
+    static class MultiThreadTester extends ThreadContextSwitchTester {
+
+        @Override
+        public void start() {
+            long start = System.currentTimeMillis();
+            
+            MyRunnable myRunnable = new MyRunnable();
+            Thread[] threads = new Thread[4];
+
+            // 创建多个线程
+            for (int i = 0; i < 4; i++) {
+                threads[i] = new Thread(myRunnable);
+                threads[i].start();
+            }
+
+            for (int i = 0; i < threads.length; i++) {
+                try {
+                    // 等待线程执行完毕
+                    threads[i].join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            long end = System.currentTimeMillis();
+            
+            System.out.printf("multi thread exec time: %d s\n", (end - start));
+            System.out.println("counter:" + super.getCounter());
+        }
+
+        class MyRunnable implements Runnable {
+
+            @Override
+            public void run() {
+                while(counter < 100000000) {
+                    synchronized (this) {
+                        if (counter < 100000000) {
+                            increaseCounter();
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    static class SerialTester extends ThreadContextSwitchTester {
+        @Override
+        public void start() {
+            long start = System.currentTimeMillis();
+
+            for (int i = 0; i < super.count; i++) {
+                super.increaseCounter();
+            }
+
+            long end = System.currentTimeMillis();
+
+            System.out.printf("serial exec time: %d s\n", (end - start));
+            System.out.println("counter:" + super.getCounter());
+        }
+    }
+
+    static abstract class ThreadContextSwitchTester {
+        public static final int count = 100000000;
+        public volatile int counter = 0;
+
+        public int getCounter() {
+            return this.counter;
+        }
+
+        public void increaseCounter() {
+            this.counter += 1;
+        }
+
+        public abstract void start();
+    }
+}
